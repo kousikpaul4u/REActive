f498cca46407fa66dc7b7203d3a2e8ccc0c42348
kousikpaul4u <kousikpaul4u@gmail.com>
2017-05-16 13:44:48 +0530

    1. Date picker added 2. CSS changes

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import ReactNative from 'react-native';
 import { connect } from 'react-redux';
+import DatePicker from 'react-native-datepicker'
+import moment from 'moment';
+
 // import * as homeStyle from '../styles/Home';
 const {
+  DatePickerIOS,
   ScrollView,
   View,
   Text,
@@ -18,17 +22,24 @@ class Home extends Component {
     this.state = {
       searching: false,
       ingredientsInput: '',
+      startDate: new Date(),
+      endDate: new Date(),
+      timeZoneOffsetInHours: (-1) * (new Date()).getTimezoneOffset() / 60,
     }
   }
   searchPressed() {
     this.setState({ searching: true });
-    this.props.fetchRecipe('02/01/2017', '05/28/2017', this.state.ingredientsInput).then(() => {
+    this.props.fetchRecipe(moment(new Date(this.state.startDate)).format('MM/DD/YYYY'),
+      moment(new Date(this.state.endDate)).format('MM/DD/YYYY'), this.state.ingredientsInput).then(() => {
       this.setState({ searching: false });
     });
   }
   recipes() {
     return Object.keys(this.props.searchedRecipes).map(key => this.props.searchedRecipes[key])
   }
+  onDateChange = (date) => {
+    this.setState({date: date});
+  };
   render() {
     console.log(this.recipes());
     return <View style={homeStyle.scene}>
@@ -43,8 +54,36 @@ class Home extends Component {
           onPress={() => this.searchPressed()}
           style={homeStyle.touchableHighlight}
         >
-          <Text> Fetched Details </Text>
+          <Text> Search </Text>
         </TouchableHighlight>
+      </View>
+      <View style={homeStyle.datePickerView}>
+      <Text>Start Date </Text>
+      <DatePicker
+          style={homeStyle.DatePicker}
+          date={this.state.startDate}
+          mode="date"
+          placeholder="placeholder"
+          format="MM/DD/YYYY"
+          minDate="01-01-2010"
+          maxDate={new Date()}
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          onDateChange={(date) => {this.setState({startDate: date});}}
+        />
+        <Text>End Date </Text>
+        <DatePicker
+            style={homeStyle.DatePicker}
+            date={this.state.endDate}
+            mode="date"
+            placeholder="placeholder"
+            format="MM/DD/YYYY"
+            minDate="01-01-2010"
+            maxDate={new Date()}
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            onDateChange={(date) => {this.setState({endDate: date});}}
+          />
       </View>
       <ScrollView style={homeStyle.scrollSection}>
         {!this.state.searching && this.recipes().map((recipe) => {
@@ -59,7 +98,7 @@ class Home extends Component {
 }
 const homeStyle = StyleSheet.create({
   scene: {
-    flex: 1,
+    flex: 2,
     marginTop: 20
   },
   searchSection: {
@@ -68,15 +107,30 @@ const homeStyle = StyleSheet.create({
     borderBottomColor: '#000',
     borderBottomWidth: 1,
     padding: 5,
+    backgroundColor: 'powderblue',
   },
   scrollSection: {
-    flex: 0.8
+    flex: 0.8,
+    backgroundColor: 'skyblue',
   },
   searchButton: {
     flex: 0.3,
   },
   searchInput: {
     flex: 0.7,
+    backgroundColor: 'powderblue',
+  },
+  datePickerView: {
+    flex: 0.1,
+    height: 30,
+    flexDirection: 'row',
+    borderBottomColor: '#000',
+    borderBottomWidth: 1,
+    padding: 5,
+    backgroundColor: 'powderblue',
+  },
+  DatePicker: {
+    width: '30%',
   },
 });
 
